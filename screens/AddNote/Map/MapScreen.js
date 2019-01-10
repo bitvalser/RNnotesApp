fee8c032db6f691db417b0c46e566325cb430c8e
bitvalser <bitvalser@gmail.com>
2019-01-11 00:29:35 +0300

    this is san francisco

--- screens/AddNote/Map/MapScreen.js
+++ screens/AddNote/Map/MapScreen.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
-import { MapView } from 'expo';
+import { StyleSheet, View, Alert, ActivityIndicator } from 'react-native';
+import { MapView, Audio } from 'expo';
+import Colors from '../../../core/constants/Colors';
 
 export default class MapScreen extends React.Component {
   static navigationOptions = {
     title: 'Map'
   };
+  soundObject = new Audio.Sound();
+  isLoading = false;
   constructor(props) {
     super(props);
     this.state = {
       coordinate: { latitude: 37.78825, longitude: -122.4324 }
     };
+  }
+  componentDidMount = () => {
+    this.playMusic();
+  }
+  playMusic = async () => {
+    try {
+      await this.soundObject.loadAsync(require('../../../assets/sounds/sanFrancisko.mp3'));
+      await this.soundObject.playAsync();
+    } catch (error) {
+      Alert.alert('Error', error);
+    }
+  };
+  componentWillUnmount = () => {
+    this.soundObject.stopAsync();
   }
   setMarker = event => {
     this.setState({
@@ -21,18 +38,22 @@ export default class MapScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-          onPress={this.setMarker}
-        >
-          <MapView.Marker coordinate={this.state.coordinate} />
-        </MapView>
+        {this.isLoading ? (
+          <ActivityIndicator style={styles.loading} size="large" color={Colors.tintColor} />
+        ) : (
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            }}
+            onPress={this.setMarker}
+          >
+            <MapView.Marker coordinate={this.state.coordinate} />
+          </MapView>
+        )}
       </View>
     );
   }
@@ -45,5 +66,8 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1
+  },
+  loading: {
+    top: '50%'
   }
 });
